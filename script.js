da46876d8e7ea814d98d393e206457e2c2594a73
NeoVerse-Tech <adebiyiabdullateef98@gmail.com>
2026-01-16 07:11:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+ function toggleHamburger() {
+   alert("Hamburger menu clicked!");
+ }
+
+ function toggleGrid() {
+   alert("Grid icon clicked!");
+ }
+
+ function toggleDots() {
+   alert("Dots menu clicked!");
+ }
